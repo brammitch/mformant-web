103046728b38417fa0c6c524783ec272bb6f6278
Brandon Mitchell <brandon.m.mitchell@gmail.com>
2019-09-12 22:38:52 -0400

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './img/logo.png';
 import './App.css';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<a
+					className="App-link"
+					href="mailto:brandon@mformant.com"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Contact
+				</a>
+			</header>
+		</div>
+	);
+};
 
 export default App;
